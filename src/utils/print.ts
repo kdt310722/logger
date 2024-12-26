76342bc69322a29fda4e55cc7430b79944c4c55f
Diep Dang <kdt310722@gmail.com>
2024-12-27 01:18:38 +0700

    feat: support multiple input types

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -2,20 +2,20 @@ import { EOL } from 'node:os'
 import { ltrim } from '@kdt310722/utils/string'
 import { bgRed, bold, dim, isColorSupported, whiteBright, yellow } from 'colorette'
 
-export function text(message: string) {
-    return whiteBright(message)
+export function text(message: any) {
+    return whiteBright(String(message))
 }
 
-export function muted(message: string) {
-    return dim(message)
+export function muted(message: any) {
+    return dim(String(message))
 }
 
-export function badge(message: string) {
+export function badge(message: any) {
     return isColorSupported ? bgRed(text(` ${message} `)) : `[${message}]`
 }
 
-export function highlight(message: string) {
-    return bold(yellow(message))
+export function highlight(message: any) {
+    return bold(yellow(String(message)))
 }
 
 export function indent(input: string, size = 2, trim = false) {
